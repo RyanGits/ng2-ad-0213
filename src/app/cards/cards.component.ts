0870096edb59309a3fc695ac8b2db2dc0999c9ac
Ryan <ryanlin108@gmail.com>
2017-02-13 14:58:23 +0800

    完成 Centric 套版 (完成 dashboard 與 cards 頁面套版)

---
+++ src/app/cards/cards.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+import {ActivatedRoute, Router} from '@angular/router';
+
+@Component({
+  selector: 'app-cards',
+  templateUrl: './cards.component.html',
+  styleUrls: ['./cards.component.css']
+})
+export class CardsComponent implements OnInit {
+
+  constructor(private router: Router, private route: ActivatedRoute) { }
+
+  ngOnInit() {
+  }
+
+}
