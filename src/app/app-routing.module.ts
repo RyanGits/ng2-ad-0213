cba8aeb1b4ea210a11b3fe978ee2ba30e95a8f7d
Ryan <ryanlin108@gmail.com>
2017-02-13 10:33:08 +0800

    路由轉向 & 萬用路由

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,8 +4,11 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 const routes: Routes = [
-  { path: '', component: LayoutComponent },
-  { path: 'page2', component: Page2Component }
+  { path: '', redirectTo: 'layout', pathMatch: 'full' },
+  { path: 'layout', component: LayoutComponent },
+  { path: 'page2', component: Page2Component },
+  { path: '**', redirectTo: '/', pathMatch: 'full' }
+
 ];
 
 @NgModule({
